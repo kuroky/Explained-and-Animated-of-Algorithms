1e8958731103cd44f475fdd9fea51603969e9a3b
kurokyfan <kuro2007cumt@gmail.com>
2018-10-19 16:52:25 +0800

    update

---
+++ 归并排序/MergeSort.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+func generateList(count: Int) -> [Int] {
+    let randomCount = count > 20 ? count : 20
+    var list: [Int] = []
+    for _ in 0..<randomCount {
+        let item = arc4random() % UInt32(randomCount)
+        list.append(Int(item))
+    }
+    return list
+}
+
+func listSort(list: inout [Int]) {
+    let n = list.count
+    if n < 2 {
+        return
+    }
+    
+    
+    
+}
+
+var items: [Int] = generateList(count: 200)
+//print("randomlist: \(items)") // 随机数组
+
+let time1 = Date().timeIntervalSince1970 * 1000
+//listSort(list: &items)
+let time2 = Date().timeIntervalSince1970 * 1000
+print("\(Int(time2 - time1)) ms")
+
+print(items)
