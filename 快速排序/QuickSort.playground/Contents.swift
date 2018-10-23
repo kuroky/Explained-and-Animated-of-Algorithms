946fa1c1e576beacd9a334b778bcf5582a320f0d
kurokyfan <kuro2007cumt@gmail.com>
2018-10-23 16:59:24 +0800

    update

--- 快速排序/QuickSort.playground/Contents.swift
+++ 快速排序/QuickSort.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 func generateList(count: Int) -> [Int] {
-    let randomCount = count > 20 ? count : 20
+    let randomCount = count > 5 ? count : 5
     var list: [Int] = []
     for _ in 0..<randomCount {
         let item = arc4random() % UInt32(randomCount)
@@ -10,13 +10,41 @@ func generateList(count: Int) -> [Int] {
     return list
 }
 
+func listSort(list: inout [Int], left: inout Int, right: inout Int) -> [Int] {
+    if left >= right {
+        return list
+    }
+    
+    let n = list.count
+    if n < 2 {
+        return list
+    }
+    
+    var low = left
+    var high = right
+    let key = list[left]
+    while left < right {
+        while left < right && list[right] >= key {
+            right -= 1
+        }
+        list[left] = list[right]
+        while left < right && list[left] <= key {
+            left += 1
+        }
+        list[right] = list[left]
+    }
+    list[left] = key
+    
+    listSort(list: &list, left: &low, right: &(left - 1))
+    listSort(list: &list, left: &(left + 1), right: &high)
+    return list
+}
 
-
-var items: [Int] = generateList(count: 200)
+var items: [Int] = generateList(count: 10)
 //print("randomlist: \(items)") // 随机数组
 
 let time1 = Date().timeIntervalSince1970 * 1000
-//listSort(list: &items)
+listSort(list: &items, left: &0, right: &(items.count - 1)
 let time2 = Date().timeIntervalSince1970 * 1000
 print("\(Int(time2 - time1)) ms")
 
